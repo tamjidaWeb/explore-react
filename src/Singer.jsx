bc850ab8d769f2e4bdde68b5d88e4fd4cbd26e45
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-12 14:12:41 +0600

    commited

--- src/Singer.jsx
+++ src/Singer.jsx
@@ -1,9 +1,18 @@
+// export default function Singer({singer}){
+//     console.log(singer);
+//     return (
+//         <div>
+//             <h3>Singer: {singer.name}</h3>
+//             <p>Age: {singer.age}</p>
+//         </div>
+//     )
+// }
 export default function Singer({singer}){
-    console.log(singer);
-    return (
-        <div>
-            <h3>Singer: {singer.name}</h3>
-            <p>Age: {singer.age}</p>
-        </div>
-    )
-}
+        console.log(singer);
+        return (
+            <div>
+                <h3>Singer: {singer.name}</h3>
+                <p>Age: {singer.age}</p>
+            </div>
+        )
+    }
