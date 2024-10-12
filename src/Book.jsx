a46e61a64a015ac267122060fd0f9142ba63c33a
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-12 14:35:17 +0600

    commited

--- src/Book.jsx
+++ src/Book.jsx
@@ -1,7 +1,8 @@
+import './Book.css';
 export default function Book ({book}){
     const {name, price} =book;
     return (
-        <div>
+        <div className='book'>
             <h3>Book Name: {name}</h3>
             <p>Price: {price}</p>
         </div>
